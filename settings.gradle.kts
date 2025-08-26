c0d02725550695c2dbbed1bbcacc9a59d8cb4953
Jakub Firkowski <firkowskikuba@gmail.com>
2025-08-26 03:10:39 +0200

    init ktor project

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "Ktor Serwer"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.firek
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}
